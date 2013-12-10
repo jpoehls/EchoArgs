038158bff36189051a469fed8563d3d196518955
Joshua Poehls <joshua@zduck.com>
2013-12-10 13:27:40 -0600

    Updated echoargs.cmd
    to include %CmdCmdLine% and have more consistent output

--- echoargs.cmd
+++ echoargs.cmd
@@ -1,10 +1,17 @@
 @echo off
-setlocal ENABLEDELAYEDEXPANSION
 
-echo [0]: %0
+echo # %%CmdCmdLine%%
+echo %CmdCmdLine%
+echo.
 
+echo # %%0
+echo %0
+echo.
+
+echo # %%*
 set /a c=1
 
+setlocal ENABLEDELAYEDEXPANSION
 FOR %%A IN (%*) DO (
 	echo [!c!]: %%A
 	set /a c=!c! + 1
