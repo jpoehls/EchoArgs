f68ba855fb8ddaec1277bd6ab0aaa3eed1f722f3
Joshua Poehls <joshua@zduck.com>
2013-12-10 13:29:03 -0600

    Updated EchoArgsCPP with more consistent output
    and include GetCommandLineW() and CommandLineToArgvW() which parses with
    different logic than argv

--- EchoArgsCPP/main.cpp
+++ EchoArgsCPP/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <Windows.h>
+
 using namespace std;
 
 int main(int argc, char* argv[]) {
-   //cout << "argc = " << argc << endl;
-   for(int i = 0; i < argc; i++)
-      cout << "[" << i << "]: " << argv[i] << endl;
-   return 0;
+	cout << "# argv" << endl;
+	for(int i = 0; i < argc; i++)
+		cout << "[" << i << "]: " << argv[i] << endl;
+
+	cout << endl;
+
+	LPCWSTR szCmdLine = GetCommandLineW();
+	cout << "# GetCommandLineW()" << endl;
+	printf("%ls", szCmdLine);
+	cout << endl;
+
+	cout <<endl;
+	cout << "# CommandLineToArgvW()" << endl;
+
+	LPWSTR *argv2 = CommandLineToArgvW(szCmdLine, &argc);
+	for (int i = 0; i < argc; i++) {
+		cout << "[" << i << "]: ";
+		printf("%ls", argv2[i]);
+		cout << endl;
+	}
+
+	return 0;
 } 
