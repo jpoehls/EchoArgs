d685759a4312afdfc19572f69bfda17c8fb0a9cf
Joshua Poehls <joshua@zduck.com>
2013-12-10 11:37:40 -0600

    Initial add of C++, C#, and batch file echo arg apps.

---
+++ EchoArgsCPP/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+   //cout << "argc = " << argc << endl;
+   for(int i = 0; i < argc; i++)
+      cout << "[" << i << "]: " << argv[i] << endl;
+   return 0;
+} 
---
+++ EchoArgsCS/main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EchoArgsCS
+{
+    internal class main
+    {
+        static void Main()
+        {
+            var args = Environment.GetCommandLineArgs();
+            for (var i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("[{0}]: {1}", i, args[i]);
+            }
+        }
+    }
+}
