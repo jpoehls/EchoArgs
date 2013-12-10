455f6b7b54304c3be73edf0eba8f6185d0375bed
Joshua Poehls <joshua@zduck.com>
2013-12-10 11:46:11 -0600

    Added PowerShell echo args script.

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ Simple applications for testing argument passing.
 * C++ app
 * C# app
 * Batch file
+* PowerShell script
 
 Each app/script will echo all arguments passed in.
---
+++ echoargs.ps1
@@ -0,0 +1,17 @@
+﻿echo "# `$PSCommandPath and `$args"
+echo "[0]: $PSCommandPath"
+
+$c = 1
+foreach ($a in $args) {
+    echo "[$c]: $a"
+    $c++
+}
+
+echo ""
+
+echo "# [System.Environment]::GetCommandLineArgs()"
+$c = 0
+foreach ($a in ([System.Environment]::GetCommandLineArgs())) {
+    echo "[$c]: $a"
+    $c++
+}
